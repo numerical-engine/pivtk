78a446ddae7cbe7a718098fafa8c3c18ad7a59f8
y.nakanishi <y.nakanishi@aixtal.com>
2025-04-23 14:45:39 +0900

    数値データ読み込みだけ、別途関数を作成

--- pivtk/In.py
+++ pivtk/In.py
@@ -20,33 +20,11 @@ def read(filename:str)->any:
     else:
         raise NotImplementedError
 
-def read_UnstructuredGrid(lines:list[str])->geom.unstructured_grid:
+
+def read_data(lines:list[str], current_idx:int, point_num:int, cell_num:int)->tuple[list[dict]]:
+    line_num = len(lines)
     point_data = []
     cell_data = []
-    current_idx = 0
-    line_num = len(lines)
-    point_num = int(lines[current_idx].split(" ")[1])
-    current_idx += 1
-
-    points = []
-    for _ in range(point_num):
-        points.append(np.array([float(s) for s in lines[current_idx].split(" ")]))
-        current_idx += 1
-    points = np.stack(points)
-
-    cell_num = int(lines[current_idx].split(" ")[1])
-    current_idx += 1
-
-    cells = []
-    for _ in range(cell_num):
-        indice = np.array([int(l) for l in lines[current_idx].split(" ")[1:]])
-        cells.append({"indice":indice})
-        current_idx += 1
-    current_idx += 1 #skip "CELL_TYPES ** line"
-    for i in range(cell_num):
-        cells[i]["type"] = int(lines[current_idx])
-        current_idx += 1
-    
     while current_idx < line_num-1:
         if lines[current_idx][:10] == "POINT_DATA":
             while current_idx < line_num-1:
@@ -71,16 +49,46 @@ def read_UnstructuredGrid(lines:list[str])->geom.unstructured_grid:
                 data_type, name = re.split("[ \t]", lines[current_idx])[:2]
                 if data_type == "SCALARS":
                     current_idx += 2
-                    val = np.array([float(lines[current_idx+i]) for i in range(point_num)])
+                    val = np.array([float(lines[current_idx+i]) for i in range(cell_num)])
                     cell_data.append({"name":name, "type":"scalar", "values":val})
                     current_idx += point_num-1
 
                 elif data_type == "VECTORS":
                     current_idx += 1
-                    val = np.stack([np.array([float(v) for v in re.split("[ \t]", lines[current_idx+i])]) for i in range(point_num)], axis = 0)
+                    val = np.stack([np.array([float(v) for v in re.split("[ \t]", lines[current_idx+i])]) for i in range(cell_num)], axis = 0)
                     cell_data.append({"name":name, "type":"vector", "values":val})
                     current_idx += point_num - 1
                 else:
                     break
+    
+    return point_data, cell_data
+
+
+def read_UnstructuredGrid(lines:list[str])->geom.unstructured_grid:
+    current_idx = 0
+    point_num = int(lines[current_idx].split(" ")[1])
+    current_idx += 1
+
+    points = []
+    for _ in range(point_num):
+        points.append(np.array([float(s) for s in lines[current_idx].split(" ")]))
+        current_idx += 1
+    points = np.stack(points)
+
+    cell_num = int(lines[current_idx].split(" ")[1])
+    current_idx += 1
+
+    cells = []
+    for _ in range(cell_num):
+        indice = np.array([int(l) for l in lines[current_idx].split(" ")[1:]])
+        cells.append({"indice":indice})
+        current_idx += 1
+    current_idx += 1 #skip "CELL_TYPES ** line"
+    for i in range(cell_num):
+        cells[i]["type"] = int(lines[current_idx])
+        current_idx += 1
+    
+    point_data, cell_data = read_data(lines, current_idx, point_num, cell_num)
+
     
     return geom.unstructured_grid(points, cells, point_data, cell_data)
